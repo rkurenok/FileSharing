848c46b86cedb89ed3c656bc9cbeecd7578a331f
Roma <rkurenok99@gmail.com>
2020-05-16 16:11:22 +0300

    added registration and authorization using forms

---
+++ FileSharing/Controllers/AccountController.cs
@@ -0,0 +1,87 @@
+﻿using FileSharing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FileSharing.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // поиск пользователя в бд
+                User user = null;
+                using (UserContext db = new UserContext())
+                {
+                    user = db.Users.FirstOrDefault(u => u.Login == model.Login && u.Password == model.Password);
+                }
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Login, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+            }
+
+            return View(model);
+        }
+
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = null;
+                using (UserContext db = new UserContext())
+                {
+                    user = db.Users.FirstOrDefault(u => u.Login == model.Login);
+                }
+                if (user == null)
+                {
+                    // создаем нового пользователя
+                    using (UserContext db = new UserContext())
+                    {
+                        db.Users.Add(new User { Login = model.Login, Password = model.Password });
+                        db.SaveChanges();
+
+                        user = db.Users.Where(u => u.Login == model.Login && u.Password == model.Password).FirstOrDefault();
+                    }
+                    // если пользователь удачно добавлен в бд
+                    if (user != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Login, true);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователь с таким логином уже существует");
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ FileSharing/Models/AccountModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FileSharing.Models
+{
+    public class LoginModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        //public int Age { get; set; }
+    }
+}
