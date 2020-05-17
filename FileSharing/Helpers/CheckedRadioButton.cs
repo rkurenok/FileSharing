20b2782dc821f0b7a591fc21888e87fcfe479461
Roma <rkurenok99@gmail.com>
2020-05-17 21:52:48 +0300

    fix view for edit account

---
+++ FileSharing/Helpers/CheckedRadioButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace FileSharing.Helpers
+{
+    public static class CheckedRadioButton
+    {
+        public static MvcHtmlString CheckedRadioButtonFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, object value)
+        {
+            return CheckedRadioButtonFor(htmlHelper, expression, value, null);
+        }
+
+        public static MvcHtmlString CheckedRadioButtonFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, object value, object htmlAttributes)
+        {
+            var func = expression.Compile();
+            var attributes = new RouteValueDictionary(htmlAttributes);
+            //var attributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+            if ((object)func(htmlHelper.ViewData.Model) == value)
+            {
+                attributes["checked"] = "checked";
+            }
+            return htmlHelper.RadioButtonFor(expression, value, attributes);
+        }
+    }
+}
