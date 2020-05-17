1ebc16710b0695e9ea0a647c69496624aa6e97d1
Roma <rkurenok99@gmail.com>
2020-05-17 12:47:44 +0300

    added view and method for change password

--- FileSharing/Controllers/ManageController.cs
+++ FileSharing/Controllers/ManageController.cs
@@ -23,5 +23,41 @@ namespace FileSharing.Controllers
 
             return View(user);
         }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassword(ChangePassword model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            User user = null;
+
+            using (UserContext db = new UserContext())
+            {
+                var userName = User.Identity.Name;
+                user = db.Users.FirstOrDefault(u => u.Login == userName);
+
+                if (user.Password == model.OldPassword)
+                {
+                    user.Password = model.NewPassword;
+                    db.SaveChanges();
+
+                    user = db.Users.Where(u => u.Login == userName && u.Password == model.NewPassword).FirstOrDefault();
+
+                    if (user != null)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View(model);
+        }
     }
 }
