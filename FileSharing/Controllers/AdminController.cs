522e42ca2451ea3a51a5205641f1b45feb765dc7
Roma <rkurenok99@gmail.com>
2020-05-17 15:14:01 +0300

    added admin panel

---
+++ FileSharing/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using FileSharing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileSharing.Controllers
+{
+    public class AdminController : Controller
+    {
+        UserContext db = new UserContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            IEnumerable<User> users = db.Users;
+
+            return View(users);
+        }
+    }
+}
