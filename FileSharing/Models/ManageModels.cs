77a87270615a56ce26abc015bafa5526f0aa73de
Roma <rkurenok99@gmail.com>
2020-05-17 18:06:01 +0300

    added validation

--- FileSharing/Models/ManageModels.cs
+++ FileSharing/Models/ManageModels.cs
@@ -8,18 +8,18 @@ namespace FileSharing.Models
 {
     public class ChangePassword
     {
-        [Required]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [DataType(DataType.Password)]
         [Display(Name = "Текущий пароль")]
         public string OldPassword { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [StringLength(100, ErrorMessage = "Значение {0} должно содержать символов не менее: {2}.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Новый пароль")]
         public string NewPassword { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [DataType(DataType.Password)]
         [Display(Name = "Подтверждение нового пароля")]
         [Compare("NewPassword", ErrorMessage = "Новый пароль и его подтверждение не совпадают.")]
@@ -28,21 +28,21 @@ namespace FileSharing.Models
 
     public class EditAccount
     {
-        [Required]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Электронная почта")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [DataType(DataType.Text)]
         [Display(Name = "Логин")]
         public string Login { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [Display(Name = "Возраст")]
         public int Age { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [Display(Name = "Пол")]
         public string Male { get; set; }
     }
