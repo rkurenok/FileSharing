23c128154975fe0de0910fce1980a7a35338e177
Roma <rkurenok99@gmail.com>
2020-05-18 18:37:52 +0300

    added feature to change the user's role

--- FileSharing/Controllers/AdminController.cs
+++ FileSharing/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace FileSharing.Controllers
 {
@@ -37,6 +38,37 @@ namespace FileSharing.Controllers
             IEnumerable<User> users = db.Users;
 
             return View("Index", users);
+        }
+
+        public ActionResult EditUserRole(int userId)
+        {
+            User user = db.Users.FirstOrDefault(u => u.Id == userId);
+
+            ViewBag.Login = user.Login;
+            ViewBag.RoleId = user.RoleId;
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditUserRole(EditRole model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            User user = db.Users.FirstOrDefault(u => u.Login == model.Login);
+            user.RoleId = model.Role;
+            db.SaveChanges();
+            user = db.Users.Where(u => u.Login == model.Login && u.RoleId == model.Role).FirstOrDefault();
+
+            if (user != null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
     }
 }
