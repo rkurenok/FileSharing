d68034b8a02da7932509c810b11e19544cbd14ce
Roma <rkurenok99@gmail.com>
2020-05-19 16:07:55 +0300

    added file upload

--- FileSharing/Controllers/HomeController.cs
+++ FileSharing/Controllers/HomeController.cs
@@ -9,6 +9,11 @@ namespace FileSharing.Controllers
 {
     public class HomeController : Controller
     {
+        public const string INTERNAL_FILE_PATH = @"E:\Универ\8 семестр\Диплом\FileSharing\FileSharing\FileSharing\Content\Files\";
+        public const string FILE_URL = "/Content/Files/";
+        public const int FILE_FRAME_SIZE = 1048576; // 1024 * 1024;
+        public const int FILE_ENSURANCE = 16;
+
         public ActionResult Index()
         {
             string result = "Вы не авторизованы";
@@ -21,6 +26,23 @@ namespace FileSharing.Controllers
             ViewBag.Result = result;
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(IEnumerable<HttpPostedFileBase> uploads)
+        {
+            foreach (var file in uploads)
+            {
+                if (file != null)
+                {
+                    // получаем имя файла
+                    string fileName = System.IO.Path.GetFileName(file.FileName);
+                    // сохраняем файл в папку Files в проекте
+                    file.SaveAs(Server.MapPath("~/Content/Files/" + fileName));
+                }
+            }
+
+            return RedirectToAction("Index");
         }
 
         [Authorize(Roles = "admin")]
