8bd9627ab3f198df18e22f47d25caee3739d362d
Roma <rkurenok99@gmail.com>
2020-05-19 13:48:09 +0300

    added sort users in admin panel

--- FileSharing/Controllers/AdminController.cs
+++ FileSharing/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
 ﻿using FileSharing.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.SqlServer;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -13,12 +15,55 @@ namespace FileSharing.Controllers
     {
         UserContext db = new UserContext();
         // GET: Admin
-        public ActionResult Index(int page = 1)
+        public ActionResult Index(string item, int page = 1)
         {
             //IEnumerable<User> users = db.Users;
 
+            string property = "";
+            //User user = db.Users.FirstOrDefault(u => u.Id == id);
+            User user = new User();
+            PropertyInfo[] properties = user.GetType().GetProperties();
+            foreach (PropertyInfo prop in properties)
+            {
+                property += prop.Name + " ";
+            }
+
+            string[] masProperty = property.Split(' ');
+
+            SelectList items = new SelectList(new List<string>(masProperty));
+            ViewData["Items"] = items;
+
+            //int pageSize = 3;
+            //IEnumerable<User> usersPerPage = db.Users.OrderBy(x => x.Id).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
             int pageSize = 3;
-            IEnumerable<User> usersPerPage = db.Users.OrderBy(x => x.Id).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            IEnumerable<User> usersPerPage = null;
+
+            switch (item)
+            {
+                case "Id":
+                    usersPerPage = db.Users.OrderBy(u => u.Id).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                    break;
+                case "Email":
+                    usersPerPage = db.Users.OrderBy(u => u.Email).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                    break;
+                case "Login":
+                    usersPerPage = db.Users.OrderBy(u => u.Login).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                    break;
+                case "Age":
+                    usersPerPage = db.Users.OrderBy(u => u.Age).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                    break;
+                case "Gender":
+                    usersPerPage = db.Users.OrderBy(u => u.Gender).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                    break;
+                case "RoleId":
+                    usersPerPage = db.Users.OrderBy(u => u.RoleId).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                    break;
+                default:
+                    usersPerPage = db.Users.OrderBy(u => u.Id).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                    break;
+            }
+
             PageInfo pageInfo = new PageInfo { PageNumber = page, PageSize = pageSize, TotalItems = db.Users.Count() };
             PageViewModel pvm = new PageViewModel { PageInfo = pageInfo, Users = usersPerPage };
 
@@ -27,6 +72,59 @@ namespace FileSharing.Controllers
 
             return View(pvm);
         }
+
+        //[HttpPost]
+        //public ActionResult Index(string item, int page = 1)
+        //{
+        //    string property = "";
+        //    //User user = db.Users.FirstOrDefault(u => u.Id == id);
+        //    User user = new User();
+        //    PropertyInfo[] properties = user.GetType().GetProperties();
+        //    foreach (PropertyInfo prop in properties)
+        //    {
+        //        property += prop.Name + " ";
+        //    }
+
+        //    string[] masProperty = property.Split(' ');
+
+        //    SelectList items = new SelectList(new List<string>(masProperty));
+        //    ViewData["Items"] = items;
+
+        //    int pageSize = 3;
+        //    IEnumerable<User> usersPerPage = null;
+
+        //    switch (item)
+        //    {
+        //        case "Id":
+        //            usersPerPage = db.Users.OrderBy(u => u.Id).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+        //            break;
+        //        case "Email":
+        //            usersPerPage = db.Users.OrderBy(u => u.Email).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+        //            break;
+        //        case "Login":
+        //            usersPerPage = db.Users.OrderBy(u => u.Login).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+        //            break;
+        //        case "Age":
+        //            usersPerPage = db.Users.OrderBy(u => u.Age).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+        //            break;
+        //        case "Gender":
+        //            usersPerPage = db.Users.OrderBy(u => u.Gender).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+        //            break;
+        //        case "RoleId":
+        //            usersPerPage = db.Users.OrderBy(u => u.RoleId).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+        //            break;
+        //        default:
+        //            usersPerPage = db.Users.OrderBy(u => u.Id).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+        //            break;
+        //    }
+
+        //    //int pageSize = 3;
+        //    //IEnumerable<User> usersPerPage = db.Users.OrderBy(u => u.Id).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+        //    PageInfo pageInfo = new PageInfo { PageNumber = page, PageSize = pageSize, TotalItems = db.Users.Count() };
+        //    PageViewModel pvm = new PageViewModel { PageInfo = pageInfo, Users = usersPerPage };
+
+        //    return View(pvm);
+        //}
 
 
         public ActionResult DetailsAccount(int userId)
@@ -77,6 +175,23 @@ namespace FileSharing.Controllers
                 return RedirectToAction("Index");
             }
             return View(model);
+        }
+
+        public string Filter(int id)
+        {
+            string property = "";
+            string result = "";
+            User user = db.Users.FirstOrDefault(u => u.Id == id);
+            PropertyInfo[] properties = user.GetType().GetProperties();
+            foreach (PropertyInfo prop in properties)
+            {
+                property += prop.Name + " ";
+            }
+
+            string[] masProperty = property.Split(' ');
+
+            SelectList items = new SelectList(new List<string>(masProperty));
+            return result;
         }
     }
 }
