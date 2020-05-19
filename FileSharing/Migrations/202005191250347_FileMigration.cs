ab04de911abad7576ea3bc29998949af6e0c9869
Roma <rkurenok99@gmail.com>
2020-05-19 15:53:39 +0300

    added file migration

---
+++ FileSharing/Migrations/202005191250347_FileMigration.cs
@@ -0,0 +1,32 @@
+﻿namespace FileSharing.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FileMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Files",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        SizeInBytes = c.Long(nullable: false),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Files", "UserId", "dbo.Users");
+            DropIndex("dbo.Files", new[] { "UserId" });
+            DropTable("dbo.Files");
+        }
+    }
+}
