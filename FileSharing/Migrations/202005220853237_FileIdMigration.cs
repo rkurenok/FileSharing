c1e8787b11d2f38c63105c08403673cd40be9768
Roma <rkurenok99@gmail.com>
2020-05-22 11:54:23 +0300

    added fileId migration

---
+++ FileSharing/Migrations/202005220853237_FileIdMigration.cs
@@ -0,0 +1,20 @@
+﻿namespace FileSharing.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FileIdMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Files", "Date", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Users", "FileId", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "FileId");
+            DropColumn("dbo.Files", "Date");
+        }
+    }
+}
