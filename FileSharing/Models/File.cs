ac0aa3908e55182650be24611ad2018343b714c0
Roma <rkurenok99@gmail.com>
2020-05-23 16:18:00 +0300

    added access model

--- FileSharing/Models/File.cs
+++ FileSharing/Models/File.cs
@@ -33,5 +33,14 @@ namespace FileSharing.Models
 
         public int? UserId { get; set; }
         public User User { get; set; }
+
+        public int AccessId { get; set; }
+        public FileAccess Access { get; set; }
+    }
+
+    public class FileAccess
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
