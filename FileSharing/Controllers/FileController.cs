01a2e93f72a9036ea13b2aaa228ed0cd267a614b
Roma <rkurenok99@gmail.com>
2020-05-24 19:11:31 +0300

    added paginated navigation in the files section

--- FileSharing/Controllers/FileController.cs
+++ FileSharing/Controllers/FileController.cs
@@ -18,13 +18,19 @@ namespace FileSharing.Controllers
         public const int FILE_ENSURANCE = 16;
 
         // GET: File
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
             //IEnumerable<File> files = db.Files.ToList();
             var files = db.Files.Include(f => f.User).Where(f => f.AccessId == 2);
             ViewBag.FileAccess = db.FileAccesses;
 
-            return View(files);
+            int pageSize = 3;
+            IEnumerable<File> filesPerPage = db.Files.OrderBy(f => f.Id).Include(f => f.User).Where(f => f.AccessId == 2).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            PageInfo pageInfo = new PageInfo { PageNumber = page, PageSize = pageSize, TotalItems = files.Count() };
+            PageViewModel pvm = new PageViewModel { PageInfo = pageInfo, Files = filesPerPage };
+
+            return View(pvm);
         }
 
         [HttpPost]
@@ -104,7 +110,7 @@ namespace FileSharing.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        public ActionResult Delete(int fileId)
+        public ActionResult Delete(int fileId, int? page)
         {
             File file = db.Files.FirstOrDefault(f => f.Id == fileId);
             string fileName = file.OriginalName;
@@ -118,7 +124,7 @@ namespace FileSharing.Controllers
 
             IEnumerable<File> files = db.Files.ToList();
 
-            return View("Index", files);
+            return RedirectToAction("FileList", "Admin", files);
         }
 
         public ActionResult Details(int id)
