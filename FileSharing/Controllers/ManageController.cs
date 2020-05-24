287b0084af5e06c522a7a07a736e47a036193401
Roma <rkurenok99@gmail.com>
2020-05-24 13:22:06 +0300

    added edit file model & displaying edit file

--- FileSharing/Controllers/ManageController.cs
+++ FileSharing/Controllers/ManageController.cs
@@ -88,6 +88,7 @@ namespace FileSharing.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult EditAccount(EditAccount model)
         {
             if (!ModelState.IsValid)
@@ -117,6 +118,46 @@ namespace FileSharing.Controllers
                     return RedirectToAction("Index");
                 }
             }
+            return View(model);
+        }
+
+        public ActionResult EditFile(int fileId)
+        {
+            File file = db.Files.FirstOrDefault(f => f.Id == fileId);
+            User user = db.Users.FirstOrDefault(u => u.Login == User.Identity.Name);
+            EditFile editFile = new EditFile { Id = file.Id, Name = file.Name, AccessId = file.AccessId };
+
+            if (user == null || file.User != user)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Name = file.Name;
+            ViewBag.AccessId = file.AccessId;
+
+            return View(editFile);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditFile(EditFile model, int fileId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            File file = db.Files.FirstOrDefault(f => f.Id == fileId);
+            file.Name = model.Name;
+            file.AccessId = model.AccessId;
+            db.SaveChanges();
+
+            file = db.Files.Where(f => f.Name == model.Name && f.AccessId == model.AccessId).FirstOrDefault();
+
+            if (file != null)
+            {
+                return RedirectToAction("Index");
+            }
+            
             return View(model);
         }
     }
